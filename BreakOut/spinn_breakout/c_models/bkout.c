03f7a7b3fe09d26fb6b6d92975c1ab5a76b474aa
neworderofjamie <james.knight@manchester.ac.uk>
2016-10-04 13:37:46 +0100

    Sorry @sfurber - long lines wrap horribly in github code-review view

--- BreakOut/spinn_breakout/c_models/bkout.c
+++ BreakOut/spinn_breakout/c_models/bkout.c
@@ -23,13 +23,18 @@
 // Macros
 //----------------------------------------------------------------------------
 // **TODO** many of these magic numbers should be passed from Python
-#define GAME_WIDTH  160                         // Game dimension constants
+// Game dimension constants
+#define GAME_WIDTH  160
 #define GAME_HEIGHT 128
 
-#define OUT_OF_PLAY 100                         // Ball outof play time (frames)
-#define FRAME_DELAY 20                          // Frame delay (ms)
+// Ball outof play time (frames)
+#define OUT_OF_PLAY 100
 
-#define FACT 16                                 // ball position and velocity scale factor
+// Frame delay (ms)
+#define FRAME_DELAY 20
+
+// ball position and velocity scale factor
+#define FACT 16
 
 //----------------------------------------------------------------------------
 // Enumerations
@@ -43,32 +48,46 @@ typedef enum
 
 typedef enum
 {
-  COLOUR_BACKGROUND = 0x1,
-  COLOUR_BAT        = 0xE,
-  COLOUR_BALL       = 0xF,
-  COLOUR_SCORE      = 0x6,
+  COLOUR_HARD       = 0x8,
+  COLOUR_SOFT       = 0x0,
+
+  COLOUR_BACKGROUND = COLOUR_SOFT | 0x1,
+  COLOUR_BAT        = COLOUR_HARD | 0x6,
+  COLOUR_BALL       = COLOUR_HARD | 0x7,
+  COLOUR_SCORE      = COLOUR_SOFT | 0x6,
 } colour_t;
 
 //----------------------------------------------------------------------------
 // Globals
 //----------------------------------------------------------------------------
-static int x = (GAME_WIDTH/4)*FACT;                    // initial ball coordinates in fixed-point
-static int y = (GAME_HEIGHT/2)*FACT;
+// initial ball coordinates in fixed-point
+static int x = (GAME_WIDTH / 4) * FACT;
+static int y = (GAME_HEIGHT / 2) * FACT;
 
-static int u = 1*FACT;                                 // initial ball velocity in fixed-point
-static int v = -1*FACT;
+// initial ball velocity in fixed-point
+static int u = 1 * FACT;
+static int v = -1 * FACT;
 
-static int x_bat   = 0;                                // bat LHS x position
-static int bat_len = 8;                                // bat length in pixels
+// bat LHS x position
+static int x_bat   = 0;
 
-static int frame_buff[GAME_WIDTH/8][GAME_HEIGHT];      // frame buffer: 160 x 128 x 4 bits: [hard/soft, R, G, B]
+// bat length in pixels
+static int bat_len = 8;
 
-static int out_of_play      = 0;                       // control pause when ball out of play
-static int score            = 0;                       // game score
+// frame buffer: 160 x 128 x 4 bits: [hard/soft, R, G, B]
+static int frame_buff[GAME_WIDTH / 8][GAME_HEIGHT];
 
-static int keystate         = 2;                       // state of left/right keys
+// control pause when ball out of play
+static int out_of_play      = 0;
 
-static const uint16_t digit[10] = {0x7B6F,0x1249,0x73E7,0x73CF,0x4979,0x79CF,0x79EF,0x7249,0x7BEF,0x7BCF};     // digits 0-9 in 3 x 5 format for score
+// game score
+static int score            = 0;
+
+// state of left/right keys
+static int keystate         = 2;
+
+// digits 0-9 in 3 x 5 format for score
+static const uint16_t digit[10] = {0x7B6F,0x1249,0x73E7,0x73CF,0x4979,0x79CF,0x79EF,0x7249,0x7BEF,0x7BCF};
 
 //! The upper bits of the key value that model should transmit with
 static uint32_t key;
@@ -87,17 +106,17 @@ static uint32_t tick_in_frame = 0;
 //----------------------------------------------------------------------------
 static inline void add_event (int i, int j, colour_t col)
 {
-    const uint32_t colour_bit = (col == COLOUR_BACKGROUND) ? 0 : 1;
-    const uint32_t spike_key = key | (i << 9) | (j << 1) | colour_bit;
+  const uint32_t colour_bit = (col == COLOUR_BACKGROUND) ? 0 : 1;
+  const uint32_t spike_key = key | (i << 9) | (j << 1) | colour_bit;
 
-    spin1_send_mc_packet(spike_key, 0, NO_PAYLOAD);
-    log_debug("%d, %d, %u, %08x\n", i, j, col, spike_key);
+  spin1_send_mc_packet(spike_key, 0, NO_PAYLOAD);
+  log_debug("%d, %d, %u, %08x\n", i, j, col, spike_key);
 }
 
 // gets pixel colour from within word
 static inline colour_t get_pixel_col (int i, int j)
 {
-    return (colour_t)(frame_buff[i/8][j] >> ((i%8)*4) & 0xF);
+  return (colour_t)(frame_buff[i/8][j] >> ((i%8)*4) & 0xF);
 }
 
 // inserts pixel colour within word
@@ -105,8 +124,8 @@ static inline void set_pixel_col (int i, int j, colour_t col)
 {
     if (col != get_pixel_col(i, j))
     {
-        frame_buff[i/8][j] = (frame_buff[i/8][j] & ~(0xF << ((i%8)*4))) | ((int)col << ((i%8)*4));
-        add_event (i, j, col);
+      frame_buff[i/8][j] = (frame_buff[i/8][j] & ~(0xF << ((i%8)*4))) | ((int)col << ((i%8)*4));
+      add_event (i, j, col);
     }
 }
 
@@ -116,112 +135,192 @@ static inline void set_pixel_col (int i, int j, colour_t col)
 // initialise frame buffer to blue
 static void init_frame ()
 {
-    for (int i=0; i<(GAME_WIDTH/8); i++)
+  for (int i=0; i<(GAME_WIDTH/8); i++)
+  {
+    for (int j=0; j<GAME_HEIGHT; j++)
     {
-        for (int j=0; j<GAME_HEIGHT; j++)
-        {
-            frame_buff[i][j] = 0x11111111 * COLOUR_BACKGROUND;
-        }
+      frame_buff[i][j] = 0x11111111 * COLOUR_BACKGROUND;
     }
+  }
 }
 
 static void update_frame ()
 {
 // draw bat
-    int old_xbat = x_bat;
-    if (keystate & 1) if (--x_bat < 0) x_bat = 0;                                                       // move bat
-    if (keystate & 2) if (++x_bat > GAME_WIDTH-bat_len) x_bat = GAME_WIDTH-bat_len;
-    if (old_xbat != x_bat) {
-        for (int i=x_bat; i<(x_bat+bat_len); i++) set_pixel_col(i, GAME_HEIGHT-1, COLOUR_BAT);             // draw yellow bat
-        if (x_bat > old_xbat)                     set_pixel_col(old_xbat, GAME_HEIGHT-1, COLOUR_BACKGROUND);
-        else if (x_bat < old_xbat)                set_pixel_col(old_xbat+bat_len, GAME_HEIGHT-1, COLOUR_BACKGROUND);
+    // Cache old bat position
+    const int old_xbat = x_bat;
+
+    // Update bat and clamp
+    if (keystate & 1 && --x_bat < 0)
+    {
+      x_bat = 0;
+    }
+    else if (keystate & 2 && ++x_bat > GAME_WIDTH-bat_len)
+    {
+      x_bat = GAME_WIDTH-bat_len;
+    }
+
+    // If bat's moved
+    if (old_xbat != x_bat)
+    {
+      // Draw bat pixels
+      for (int i = x_bat; i < (x_bat + bat_len); i++)
+      {
+        set_pixel_col(i, GAME_HEIGHT-1, COLOUR_BAT);
+      }
+
+      // Remove pixels left over from old bat
+      if (x_bat > old_xbat)
+      {
+        set_pixel_col(old_xbat, GAME_HEIGHT-1, COLOUR_BACKGROUND);
+      }
+      else if (x_bat < old_xbat)
+      {
+        set_pixel_col(old_xbat + bat_len, GAME_HEIGHT-1, COLOUR_BACKGROUND);
+      }
     }
 
 // draw 3-digit score
     int s = score;
-    for (int k=0; k<3; k++) {
-        int s_x = GAME_WIDTH/2 + 3 - 4*k;
-        for (int i=0; i<3; i++) {
-            for (int j=0; j<5; j++) {
-                if (digit[s%10] & (1<<(14-(i+3*j)))) set_pixel_col(s_x + i, 5 + j, COLOUR_SCORE);
-                else                                 set_pixel_col(s_x + i, 5 + j, COLOUR_BACKGROUND);
-            }
+    // Loop through characters
+    for (int k = 0; k < 3; k++)
+    {
+      const int s_x = (GAME_WIDTH / 2) + 3 - (4 * k);
+
+      // Loop through x character pixels
+      for (int i=0; i<3; i++)
+      {
+        // Loop through y character pixels
+        for (int j=0; j<5; j++)
+        {
+          // Set or clear pixel based on 'font' bitmap
+          if (digit[s%10] & (1<<(14-(i+3*j))))
+          {
+            set_pixel_col(s_x + i, 5 + j, COLOUR_SCORE);
+          }
+          else
+          {
+            set_pixel_col(s_x + i, 5 + j, COLOUR_BACKGROUND);
+          }
         }
-        s = s/10;
+      }
+      s = s/10;
     }
 
 // draw ball
-    if (out_of_play == 0) {
-        set_pixel_col(x/FACT, y/FACT, COLOUR_BACKGROUND);                                                      // clear pixel to background
-        x += u;                                                                                         // move ball
-        if ((x < -u) || (x >= ((GAME_WIDTH*FACT)-u)))  u = -u;                                          // bounce off sides
-        y += v;
-        if (y < -v) v = -v;                                                                             // bounce off top
+    if (out_of_play == 0)
+    {
+      // clear pixel to background
+      set_pixel_col(x/FACT, y/FACT, COLOUR_BACKGROUND);
+
+      // move ball in x and bounce off sides
+      x += u;
+      if ((x < -u) || (x >= ((GAME_WIDTH*FACT)-u)))
+      {
+        u = -u;
+      }
+
+      // move ball in y and bounce off top
+      y += v;
+      if (y < -v)
+      {
+        v = -v;
+      }
 
 //detect collision
-        if (get_pixel_col(x/FACT, y/FACT) & 0x8) {                                                      // hit something hard!
-            if (x/FACT < (x_bat+bat_len/4))        u = -FACT;
-            else if (x/FACT < (x_bat+bat_len/2))   u = -FACT/2;
-            else if (x/FACT < (x_bat+3*bat_len/4)) u = FACT/2;
-            else                                   u = FACT;
-            v = -FACT;
-            y -= FACT;
-            if (score < 999) ++score;
+      // if we hit something hard!
+      if (get_pixel_col(x / FACT, y / FACT) & COLOUR_HARD)
+      {
+        if (x/FACT < (x_bat+bat_len/4))
+        {
+          u = -FACT;
+        }
+        else if (x/FACT < (x_bat+bat_len/2))
+        {
+          u = -FACT/2;
+        }
+        else if (x/FACT < (x_bat+3*bat_len/4))
+        {
+          u = FACT/2;
+        }
+        else
+        {
+          u = FACT;
         }
 
-// lost ball
-        if (y >= (GAME_HEIGHT*FACT-v)) {
-            v = -1*FACT;
-            y = (GAME_HEIGHT/2)*FACT;
-            out_of_play = OUT_OF_PLAY;
-            if (score > 0) --score;
-        } else {
-            set_pixel_col(x/FACT, y/FACT, COLOUR_BALL);                                                    // draw ball
+        v = -FACT;
+        y -= FACT;
+        if (score < 999)
+        {
+          ++score;
         }
-    } else --out_of_play;
+      }
+
+// lost ball
+      if (y >= (GAME_HEIGHT*FACT-v))
+      {
+        v = -1 * FACT;
+        y = (GAME_HEIGHT / 2)*FACT;
+        out_of_play = OUT_OF_PLAY;
+        if (score > 0)
+        {
+          --score;
+        }
+      }
+      // draw ball
+      else
+      {
+        set_pixel_col(x/FACT, y/FACT, COLOUR_BALL);
+      }
+    }
+    else
+    {
+      --out_of_play;
+    }
 }
 
 static bool initialize(uint32_t *timer_period)
 {
-    log_info("Initialise: started");
+  log_info("Initialise: started");
 
-    // Get the address this core's DTCM data starts at from SRAM
-    address_t address = data_specification_get_data_address();
+  // Get the address this core's DTCM data starts at from SRAM
+  address_t address = data_specification_get_data_address();
 
-    // Read the header
-    if (!data_specification_read_header(address)) {
-        return false;
-    }
+  // Read the header
+  if (!data_specification_read_header(address))
+  {
+      return false;
+  }
 
-    // Get the timing details and set up the simulation interface
-    if (!simulation_initialise(
-            data_specification_get_region(REGION_SYSTEM, address),
-            APPLICATION_NAME_HASH, timer_period, &simulation_ticks,
-            &infinite_run, 1, NULL,
-            data_specification_get_region(REGION_PROVENANCE, address))) {
-        return false;
-    }
+  // Get the timing details and set up the simulation interface
+  if (!simulation_initialise(data_specification_get_region(REGION_SYSTEM, address),
+    APPLICATION_NAME_HASH, timer_period, &simulation_ticks,
+    &infinite_run, 1, NULL, data_specification_get_region(REGION_PROVENANCE, address)))
+  {
+      return false;
+  }
 
-    // Read breakout region
-    address_t breakout_region = data_specification_get_region(REGION_BREAKOUT,
-                                                              address);
-    key = breakout_region[0];
-    log_info("\tKey=%08x", key);
+  // Read breakout region
+  address_t breakout_region = data_specification_get_region(REGION_BREAKOUT, address);
+  key = breakout_region[0];
+  log_info("\tKey=%08x", key);
 
-    log_info("Initialise: completed successfully");
+  log_info("Initialise: completed successfully");
 
-    return true;
+  return true;
 }
 
 //----------------------------------------------------------------------------
 // Callbacks
 //----------------------------------------------------------------------------
-/*void process_sdp (uint m, uint port)   {                                                                // incoming SDP message
+// incoming SDP message
+/*void process_sdp (uint m, uint port)
+*{
     sdp_msg_t *msg = (sdp_msg_t *) m;
 
     io_printf (IO_BUF, "SDP len %d, port %d - %s\n", msg->length, port, msg->data);
-
-    if (port == 1) spin1_memcpy(&keystate, msg->data, 4);                                               // Port 1 - key data
+    // Port 1 - key data
+    if (port == 1) spin1_memcpy(&keystate, msg->data, 4);
     spin1_msg_free (msg);
     if (port == 7) spin1_exit (0);
 }*/
@@ -232,7 +331,7 @@ void timer_callback(uint ticks, uint dummy)
     // **NOTE** ticks starts at 1!
     if (infinite_run != TRUE && (ticks - 1) >= simulation_ticks)
     {
-        return;
+      return;
     }
     // Otherwise
     else
@@ -245,10 +344,11 @@ void timer_callback(uint ticks, uint dummy)
         // collision detection relies on this
         if(ticks == FRAME_DELAY)
         {
-            for (int i=x_bat; i<(x_bat+bat_len); i++)
-            {
-                set_pixel_col(i, GAME_HEIGHT-1, COLOUR_BAT);
-            }
+          // Draw bat
+          for (int i=x_bat; i<(x_bat+bat_len); i++)
+          {
+            set_pixel_col(i, GAME_HEIGHT-1, COLOUR_BAT);
+          }
         }
 
         // Reset ticks in frame and update frame
@@ -263,23 +363,23 @@ void timer_callback(uint ticks, uint dummy)
 //----------------------------------------------------------------------------
 void c_main(void)
 {
-    // Load DTCM data
-    uint32_t timer_period;
-    if (!initialize(&timer_period))
-    {
-        log_error("Error in initialisation - exiting!");
-        rt_error(RTE_SWERR);
-    }
+  // Load DTCM data
+  uint32_t timer_period;
+  if (!initialize(&timer_period))
+  {
+    log_error("Error in initialisation - exiting!");
+    rt_error(RTE_SWERR);
+  }
 
-    init_frame();
-    keystate = 0;
-    tick_in_frame = 0;
+  init_frame();
+  keystate = 0;
+  tick_in_frame = 0;
 
-    // Set timer tick (in microseconds)
-    spin1_set_timer_tick(timer_period);
+  // Set timer tick (in microseconds)
+  spin1_set_timer_tick(timer_period);
 
-    // Register callback
-    spin1_callback_on(TIMER_TICK, timer_callback, 2);
+  // Register callback
+  spin1_callback_on(TIMER_TICK, timer_callback, 2);
 
-    simulation_run();
+  simulation_run();
 }
